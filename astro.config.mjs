1ee6ddef499bd236fae766934c5b81f444127184
kulachat <freelancemans@gmail.com>
2024-06-17 23:54:16 +0700

    Update Design

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://promptpage.org/',
-  integrations: [robotsTxt(), sitemap()],
+  integrations: [robotsTxt(), sitemap(), tailwind()]
 });
